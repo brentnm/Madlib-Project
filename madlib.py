070d40c4ceb6bb70071ba3cb7dcdc373db4439ce
Nathan Brent <brentnm@dukes.jmu.edu>
2021-02-17 22:07:38 -0500

    did code

---
+++ madlib.py
@@ -0,0 +1,13 @@
+blanks=[]
+word1=input("give a noun: ")
+blanks.append(word1)
+word2=input("give a proper noun: ")
+blanks.append(word2)
+word3=input("give a verb: ")
+blanks.append(word3)
+word4=input("give a verb (past tense): ")
+blanks.append(word4)
+word5=input("give a noun: ")
+blanks.append(word5)
+
+print("There once was a " + word1 + " named " + word2 + " who had to get up and go " + word3 + ", but they didn't quite make it and " + word4 + " a " + word5 + ".")
